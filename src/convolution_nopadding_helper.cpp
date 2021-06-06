e87e1c96fe120f08354a2853160893b0e3f5875d
Alexandros-Stavros Iliopoulos <1577182+ailiop@users.noreply.github.com>
2021-06-06 13:16:08 -0400

    Tentative for->cilk_for in EEE 3D conv

--- src/convolution_nopadding_helper.cpp
+++ src/convolution_nopadding_helper.cpp
@@ -25,9 +25,9 @@ void eee( double * const PhiGrid, const double *VGrid,
         Xc[i] = 0.0;
 
 // ~~~~~~~~~~~~~~~~~~~~ SETUP KERNEL
-for (uint32_t k=0; k<n3; k++) {
-  for (uint32_t j=0; j<n2; j++) {
-    for (uint32_t i=0; i<n1; i++) {
+for (long k=0; k<n3; k++) {
+  for (long j=0; j<n2; j++) {
+    for (long i=0; i<n1; i++) {
       std::complex<double> tmp( kernel3d( hsq, i, j, k ), 0 );
       Kc[SUB2IND3D(i,j,k,n1,n2)] += tmp;
       if (i>0) Kc[SUB2IND3D(n1-i,j,k,n1,n2)] += tmp;
@@ -42,17 +42,9 @@ for (uint32_t k=0; k<n3; k++) {
  }
   
 // ~~~~~~~~~~~~~~~~~~~~ SETUP RHS
-for (uint32_t iVec=0; iVec<nVec; iVec++) {
-  for (uint32_t k=0; k<n3; k++) {
-    for (uint32_t j=0; j<n2; j++) {
-      for (uint32_t i=0; i<n1; i++) {
-        Xc[ SUB2IND4D(i, j, k, iVec ,n1, n2, n3) ] =
-          VGrid[ SUB2IND4D(i, j, k, iVec, n1, n2, n3) ];
-      }
-    }
-  }
- }
 
+cilk_for (long i = 0; i < n1*n2*n3*nVec; i++)
+    Xc[i] = VGrid[i];
 
 // ---------- execute kernel plan
 fftw_execute(planc_kernel);
@@ -61,32 +53,17 @@ fftw_execute(planc_kernel);
 fftw_execute(planc_rhs);
 
 // ~~~~~~~~~~~~~~~~~~~~ HADAMARD PRODUCT
-for (uint32_t jVec=0; jVec<nVec; jVec++) {
-  for (uint32_t k=0; k<n3; k++){
-    for (uint32_t j=0; j<n2; j++){
-      for (uint32_t i=0; i<n1; i++){
-        Xc[SUB2IND4D(i,j,k,jVec,n1,n2,n3)] = Xc[SUB2IND4D(i,j,k,jVec,n1,n2,n3)] *
-          Kc[SUB2IND3D(i,j,k,n1,n2)];
-      }
-    }
-  }
- }
+cilk_for (long i = 0; i < n1*n2*n3; i++)
+    for (long jVec = 0; jVec < nVec; jVec++)
+        Xc[jVec*n1*n2*n3 + i] *= Kc[i];
 
 // ---------- execute plan
 fftw_execute(planc_inverse);
 
 // ---------- (no conjugate multiplication)
 
-for (uint32_t iVec=0; iVec<nVec; iVec++){
-  for (uint32_t k=0; k<n3; k++){
-    for (uint32_t j=0; j<n2; j++){
-      for (uint32_t i=0; i<n1; i++){
-        PhiGrid[ SUB2IND4D(i, j, k, iVec, n1, n2, n3) ] =
-          Xc[ SUB2IND4D(i, j, k, iVec, n1, n2, n3) ].real();
-      }
-    }
-  }
- }
+cilk_for (long i = 0; i < n1*n2*n3*nVec; i++)
+    PhiGrid[i] = Xc[i].real();
 
 }
 
