9be375d73c394bd59dc06babf84697c028f5f8c6
Dimitris Floros <flojim4@gmail.com>
2021-06-20 18:37:59 +0300

    Python instructions

---
+++ python/sgtsnepi.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+import numpy as np
+
+from scipy.sparse import csc_matrix
+
+from julia import Main
+from julia import Pkg
+
+# load packages in Julia
+Pkg.add( "SparseArrays" )
+Pkg.add( "SGtSNEpi" )
+
+from julia import SGtSNEpi
+
+# prepare Julia wrappers
+Main.eval("""
+  using SparseArrays, SGtSNEpi
+  import SGtSNEpi.sgtsnepi
+  function scipyCSC_to_julia(A)
+    m, n = A.shape
+    colPtr = Int[i+1 for i in PyArray(A."indptr")]
+    rowVal = Int[i+1 for i in PyArray(A."indices")]
+    nzVal = Vector{Float64}(PyArray(A."data"))
+    B = SparseMatrixCSC{Float64,Int}(m, n, colPtr, rowVal, nzVal)
+  end
+  sgtsnepi( A::PyObject ; kwargs... ) = sgtsnepi( scipyCSC_to_julia(A) ; kwargs... )
+""")
+
+# setup Python function
+
+def sgtsnepi( A, **kwargs ):
+    SGtSNEpi.sgtsnepi( A, **kwargs )
